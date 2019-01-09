8facbf925416e03bb48e2cec4389f473b14d8bda
jhun <jhun@nm-4ones.com>
2019-01-09 16:05:49 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/sse/SseApplication.java
@@ -0,0 +1,31 @@
+package com.glqdlt.ex.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+
+@RestController
+@SpringBootApplication
+public class SseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseApplication.class, args);
+    }
+
+
+    @CrossOrigin
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "Flux - " + LocalTime.now().toString());
+    }
+}
+
